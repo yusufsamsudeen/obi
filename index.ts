265835351e1f93b4bbf27c095d48058b23230ac7
Yusuf Samsudeen <yusufsamsudeen2013@gmail.com>
2021-04-27 12:02:40 +0100

    Added @Authenticated Decorator for securing routes and also refactored route creation mechanism

--- index.ts
+++ index.ts
@@ -2,4 +2,5 @@ export { Controller } from "./decorators/controller.decorator";
 export { RequestMapping } from "./decorators/requestmapping.decorator";
 export { Methods } from "./decorators/requestmethod.decorator";
 export { RequestParam} from "./decorators/requestparam.decorator"
+export { Authenticated } from "./decorators/authenticated.decorator"
 export { App } from "./app";
